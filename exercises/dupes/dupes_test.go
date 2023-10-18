99ac554bd8b3a974d83c7ebc9054dd0b823746ff
John Arundel <john@bitfieldconsulting.com>
2023-10-18 15:32:32 +0100

    add dupes

---
+++ exercises/dupes/dupes_test.go
@@ -0,0 +1,29 @@
+package dupes_test
+
+import (
+	"testing"
+
+	"github.com/bitfield/dupes"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestDupesIsTrueWhenInputContainsNonConsecutiveDuplicates(t *testing.T) {
+	t.Parallel()
+	s := []int{1, 2, 3, 1, 5}
+	want := true
+	got := dupes.Dupes(s)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestDupesIsFalseWhenInputContainsNoDuplicates(t *testing.T) {
+	t.Parallel()
+	s := []string{"a"}
+	want := false
+	got := dupes.Dupes(s)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
